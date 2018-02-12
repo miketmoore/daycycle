396bb35b73a3f10c886e34abf538c4b016409e4c
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-11 21:53:46 -0500

    Start main, add notes

---
+++ cmd/week/week.go
@@ -0,0 +1,26 @@
+package main
+
+// Week is an implementation of a finite state machine in Go.
+// Each state represents a section of time during the week.
+// I guess this is a type of clock.
+
+// Sunday
+// Monday
+// Tuesday
+// Wendesday
+// Thursday
+// Friday
+// Saturday
+
+// Dawn
+// Morning
+// Noon
+// Afternoon
+// Dusk
+// Evening
+// Night
+// Midnight
+
+func main() {
+
+}
