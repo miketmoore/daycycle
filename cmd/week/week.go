524218cd88055523e1577af2507d939b2a4bfcbf
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-11 22:13:21 -0500

    Setup logger and GUI window, loop

--- cmd/week/week.go
+++ cmd/week/week.go
@@ -1,9 +1,26 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
 // Week is an implementation of a finite state machine in Go.
 // Each state represents a section of time during the week.
 // I guess this is a type of clock.
 
+// https://english.stackexchange.com/questions/28498/precise-names-for-parts-of-a-day
+
+type InputHandler interface {
+	HandleInput(pixelgl.Window)
+}
+
+// FSM Weekdays
 // Sunday
 // Monday
 // Tuesday
@@ -12,6 +29,25 @@ package main
 // Friday
 // Saturday
 
+// type Weekday int
+
+// const (
+// 	Sunday Weekday = iota
+// 	Monday
+// 	Tuesday
+// 	Wendesday
+// 	Thursday
+// 	Friday
+// 	Saturday
+// )
+
+type StateSunday struct {
+}
+
+func (s StateSunday) HandleInput(win *pixelgl.Window) {
+}
+
+// FSM Parts of a Day
 // Dawn
 // Morning
 // Noon
@@ -21,6 +57,40 @@ package main
 // Night
 // Midnight
 
-func main() {
+func run() {
+	// Setup a logger
+	writer, logger := initLogger()
+	logger.Print("run")
+	writer.Flush()
 
+	// Setup GUI window
+	cfg := pixelgl.WindowConfig{
+		Title:  "One Week",
+		Bounds: pixel.R(0, 0, 400, 225),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Clear(colornames.Antiquewhite)
+		win.Update()
+		writer.Flush()
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+}
+
+func initLogger() (*bufio.Writer, *log.Logger) {
+	f, err := os.Create("/tmp/dat2")
+	if err != nil {
+		panic(err)
+	}
+	writer := bufio.NewWriter(f)
+	logger := log.New(writer, "INFO: ", log.Lshortfile)
+	return writer, logger
 }
